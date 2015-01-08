0687151ecf7e9b08e2d645b2534dd4e57bacdd7c
KatWenger <katherinewenger@gmail.com>
2015-01-07 19:43:38 -0500

    Initial commit

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'bundler'
+require "net/http"
 Bundler.require
 
 class MyApp < Sinatra::Base
 
   get '/' do
+  uri = URI('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
+  hash = Net::HTTP.get(uri)
+  @image = hash["data"]["image_url"]
     erb :index
   end
 
