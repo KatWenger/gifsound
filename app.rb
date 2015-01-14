2db3e0f3d5a69053a27490f2a54923197a41cd08
KatWenger <katherinewenger@gmail.com>
2015-01-14 18:28:50 -0500

    I got the gifs to work

--- app.rb
+++ app.rb
@@ -1,18 +1,17 @@
 require 'bundler'
 require "net/http"
+require 'json'
 Bundler.require
 
 class MyApp < Sinatra::Base
 
   get '/' do
-  	uri = URI('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
-  	hash = Net::HTTP.get(uri)
- 	 @image = hash["data"]["image_url"]
    		erb :index
   end
   get '/gif' do
   	uri = URI('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
-  	hash = Net::HTTP.get(uri)
+  	api_response = Net::HTTP.get(uri)
+  	hash= JSON.parse(api_response)
   	@image = hash["data"]["image_url"] 
   		erb :gif 
   end
