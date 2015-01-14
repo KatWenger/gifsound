8116455369a0b3f23b23550756eb03b4c8e61764
KatWenger <katherinewenger@gmail.com>
2015-01-14 17:03:14 -0500

    not working yet

--- app.rb
+++ app.rb
@@ -5,10 +5,15 @@ Bundler.require
 class MyApp < Sinatra::Base
 
   get '/' do
-  uri = URI('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
-  hash = Net::HTTP.get(uri)
-  @image = hash["data"]["image_url"]
-    erb :index
+  	uri = URI('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
+  	hash = Net::HTTP.get(uri)
+ 	 @image = hash["data"]["image_url"]
+   		erb :index
   end
-
+  get '/gif' do
+  	uri = URI('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
+  	hash = Net::HTTP.get(uri)
+  	@image = hash["data"]["image_url"] 
+  		erb :gif 
+  end
 end
